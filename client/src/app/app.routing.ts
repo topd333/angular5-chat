99be891916050a2965eb050ce31fe62342347644
topd333 <genius.snow0405@gmail.com>
2018-04-18 03:30:11 +0200

    built chat UI

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -4,11 +4,14 @@ import { HomeComponent } from './home';
 import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { PasswordComponent } from './password';
+import { ChatComponent } from './chat';
 import { AuthGuard, UnauthGuard } from './_guards';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'password/reset', component: PasswordComponent, canActivate: [AuthGuard] },
+  { path: 'chat', component: ChatComponent, canActivate: [AuthGuard] },
+
   { path: 'login', component: LoginComponent, canActivate: [UnauthGuard] },
   { path: 'register', component: RegisterComponent, canActivate: [UnauthGuard] },
 
